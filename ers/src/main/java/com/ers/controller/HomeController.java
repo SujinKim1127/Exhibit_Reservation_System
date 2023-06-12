91a430cf48f3e8dcd8535152dfad9c9fb577a15f
jwo0o0 <jwo0o0358@gmail.com>
2023-06-12 18:59:04 +0900

    [feat] 전시 조회 기능 구현

--- ers/src/main/java/com/ers/controller/HomeController.java
+++ ers/src/main/java/com/ers/controller/HomeController.java
@@ -1,4 +1,58 @@
 package com.ers.controller;
 
+import com.ers.model.Exhibit;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Controller
 public class HomeController {
+    RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/")
+    public String home(Model model) {
+        String url = "http://localhost:8080/exhibitlist";
+        ResponseEntity<String> response_ = restTemplate.getForEntity(url, String.class);
+        String json = response_.getBody();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<Exhibit> exhibitList = objectMapper.readValue(json, new TypeReference<List<Exhibit>>() {});
+            model.addAttribute("exhibitlist", exhibitList);
+        } catch (Exception e) {
+            // 파싱 중 오류가 발생한 경우 처리
+            e.printStackTrace();
+        }
+
+        return "main";
+    }
+
+    @GetMapping("/exhibitdetail/{exhibit_id}")
+    public String exhibitDetail(Model model, @PathVariable("exhibit_id") String exhibitId) {
+        System.out.println("전시 상세 조회" + exhibitId);
+        String url = "http://localhost:8080/exhibit?id=" + exhibitId;
+
+        ResponseEntity<String> response_ = restTemplate.getForEntity(url, String.class);
+        String json = response_.getBody();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Exhibit exhibit = objectMapper.readValue(json, new TypeReference<Exhibit>() {});
+            model.addAttribute("exhibit", exhibit);
+        } catch (Exception e) {
+            // 파싱 중 오류가 발생한 경우 처리
+            e.printStackTrace();
+        }
+
+        return "exhibitdetail";
+    }
+
 }
--- ers/src/main/webapp/WEB-INF/views/main.jsp
+++ ers/src/main/webapp/WEB-INF/views/main.jsp
@@ -1,16 +1,32 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: sujin
-  Date: 2023/06/10
-  Time: 4:42 PM
-  To change this template use File | Settings | File Templates.
---%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>main</title>
+    <title><spring:message code="exhibitlist" /></title>
 </head>
 <body>
+<h2><spring:message code="exhibitlist" /></h2>
+<table border="1" cellpadding="10">
+    <tr>
+        <th><spring:message code="exhibitTitle" /></th>
+        <th><spring:message code="start_date" /></th>
+        <th><spring:message code="end_date" /></th>
+        <th><spring:message code="price" /></th>
+        <th><spring:message code="owner" /></th>
+        <th><spring:message code="likes" /></th>
+    </tr>
+    <c:forEach items="${exhibitlist}" var="exhibit">
+        <tr>
+            <td><a href="/exhibitdetail/${exhibit.exhibit_id}">${exhibit.title}</a></td>
+            <td>${exhibit.start_date}</td>
+            <td>${exhibit.end_date}</td>
+            <td>${exhibit.price}</td>
+            <td>${exhibit.owner}</td>
+            <td>${exhibit.likes}</td>
+        </tr>
+    </c:forEach>
+</table>
 
 </body>
 </html>
