747bcb72b2ba70740a99a76a1541e4625b899fdd
jwo0o0 <jwo0o0358@gmail.com>
2023-06-13 20:36:54 +0900

    [feat] Order 모델 추가

---
+++ ers/src/main/java/com/ers/model/Order.java
@@ -0,0 +1,85 @@
+package com.ers.model;
+
+public class Order {
+    private int order_id;
+    private int user_id;
+    private int exhibit_id;
+    private int price;
+    private String purchase_date;
+    private String address;
+    private String name;
+    private String tel;
+    private int amount;
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getExhibit_id() {
+        return exhibit_id;
+    }
+
+    public void setExhibit_id(int exhibit_id) {
+        this.exhibit_id = exhibit_id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getPurchase_date() {
+        return purchase_date;
+    }
+
+    public void setPurchase_date(String purchase_date) {
+        this.purchase_date = purchase_date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
